a6d8afea71fa3bd1f8010ca4d6d9f324e0ad2c6c
Konijnebeer <98305413+Konijnebeer@users.noreply.github.com>
2025-12-02 15:40:24 +0100

    Fixes McFixes

--- resources/views/quest/index.blade.php
+++ resources/views/quest/index.blade.php
@@ -2,13 +2,14 @@
 
     <section
         id="mainSection"
-        class="bg-[url('/public/hooglander.png')] h-screen bg-no-repeat bg-cover bg-center overflow-hidden"
+        class="bg-[url('/public/images/hooglander.png')] h-[calc(100vh-175px)] bg-no-repeat bg-cover bg-center overflow-hidden"
     >
-    <h1 class="text-black m-4 text-center">Quests</h1>
+        <h1 class="text-black m-4 text-center">Quests</h1>
 
 
         <a href="" id="button">
-            <div class=" flex border-2 border-solid shadow-lg border-black rounded-5 flex-row bg-white m-4 p-4 justify-between">
+            <div
+                class="flex border-2 border-solid shadow-lg border-black rounded-2 flex-row bg-white m-4 p-4 justify-between">
 
                 <div class="items-start">
                     <h2>Stroomvreter</h2>
@@ -22,7 +23,7 @@
         </a>
 
         <a href="" id="button">
-            <div class=" flex border-2 border-solid border-black rounded-5 flex-row bg-white m-4 p-4 justify-between">
+            <div class="flex border-2 border-solid border-black rounded-2 flex-row bg-white m-4 p-4 justify-between">
 
                 <div class="items-start">
                     <h2>Stroomvreter</h2>
@@ -36,7 +37,7 @@
         </a>
 
         <a href="" id="button">
-            <div class=" flex border-2 border-solid border-black rounded-5 flex-row bg-white m-4 p-4 justify-between">
+            <div class="flex border-2 border-solid border-black rounded-2 flex-row bg-white m-4 p-4 justify-between">
 
                 <div class="items-start">
                     <h2>Stroomvreter</h2>
@@ -51,7 +52,6 @@
 
 
         <p class="text-center text-white">Kom snel terug voor meer quests!</p>
-
 
 
     </section>
