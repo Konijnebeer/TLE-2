000dd19bf9a83302edf499ca2fe02824d9741ff4
lisa-mao <1091575@hr.nl>
2025-12-02 14:50:27 +0100

    quest

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/quest', function () {
+    return view('quest');
+})->middleware(['auth', 'verified'])->name('quest');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
