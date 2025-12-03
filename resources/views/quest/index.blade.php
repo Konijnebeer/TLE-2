b1ef83bf6085bc1bb0c9a8a46998fa631036a846
Semmetje <semvandenende@gmail.com>
2025-12-03 10:32:26 +0100

    - Linked all current pages to each-other - Added favicon - Refactored quests page with questbox components for easier reusing. - Updated profile page with our styling - Updated web.php with correct routes and comments

--- resources/views/quest/index.blade.php
+++ resources/views/quest/index.blade.php
@@ -6,55 +6,12 @@
     >
         <h1 class="text-black m-4 text-center">Quests</h1>
 
-
-        <a href="" id="button">
-            <div
-                class="flex border-2 border-solid shadow-lg border-black rounded-2 flex-row bg-white m-4 p-4 justify-between">
-
-                <div class="items-start">
-                    <h2>Stroomvreter</h2>
-                    <p>Start jouw (app naam) avontuur!</p>
-                </div>
-                <div class="flex  items-center ">
-                    <p>0/1</p>
-                </div>
-
-            </div>
-        </a>
-
-        <a href="" id="button">
-            <div class="flex border-2 border-solid border-black rounded-2 flex-row bg-white m-4 p-4 justify-between">
-
-                <div class="items-start">
-                    <h2>Stroomvreter</h2>
-                    <p>Start jouw (app naam) avontuur!</p>
-                </div>
-                <div class="flex  items-center ">
-                    <p>0/1</p>
-                </div>
-
-            </div>
-        </a>
-
-        <a href="" id="button">
-            <div class="flex border-2 border-solid border-black rounded-2 flex-row bg-white m-4 p-4 justify-between">
-
-                <div class="items-start">
-                    <h2>Stroomvreter</h2>
-                    <p>Start jouw (app naam) avontuur!</p>
-                </div>
-                <div class="flex  items-center ">
-                    <p>0/1</p>
-                </div>
-
-            </div>
-        </a>
-
+        <x-questbox title="Stroomvreter" progress="0/1">
+            Start jouw avontuur!
+        </x-questbox>
 
         <p class="text-center text-white">Kom snel terug voor meer quests!</p>
 
-
     </section>
-
 
 </x-app-layout>
