ecdc1fa3b03d378a643c504c31d3a610ebe69bec
Konijnebeer <98305413+Konijnebeer@users.noreply.github.com>
2025-12-08 12:33:29 +0100

    Added creation of code, auto removing codes when invalid Styling improvements

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use App\Models\Group;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('codes:expire', function () {
+    $expiredCount = Group::where('code_expires_at', '<=', now())
+        ->whereNotNull('code')
+        ->update([
+            'code' => null,
+            'code_expires_at' => null
+        ]);
+
+    $this->info("Expired {$expiredCount} class code(s)");
+})->purpose('Manually expire all class codes that have passed their expiration date')->everyMinute()->withoutOverlapping();
