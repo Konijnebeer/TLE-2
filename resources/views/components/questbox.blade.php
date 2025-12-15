9587807c6e67c5105d573cf43a6926a493641d1c
Semmetje <semvandenende@gmail.com>
2025-12-15 15:12:28 +0100

    Enhance accessibility of quest progress display and clean up about page layout

--- resources/views/components/questbox.blade.php
+++ resources/views/components/questbox.blade.php
@@ -5,20 +5,25 @@
     'partLink' => '#'
 ])
 
+@php
+    $progressSplit = explode('/', $progress);
+@endphp
+
 <div
     class=" border-2 border-solid shadow-lg border-black rounded-2 flex-row bg-white m-4 p-4 justify-between cursor-pointer hover:shadow-xl transition-shadow"
     onclick="window.location='{{ $questLink }}'">
 
     <div class="flex justify-between items-center mb-4">
         <h2 class="text-3xl">{{ $title }}</h2>
-        <p class="text-md font-semibold">{{ $progress }}</p>
+        <p class="text-md font-semibold" aria-hidden="true">{{ $progress }}</p>
+        <p class="sr-only">Progressie: {{ $progressSplit[0] }} van de {{ $progressSplit[1] }}</p>
         {{--        <p class="">{{ $slot }}</p>--}}
     </div>
 
 
     <x-button
         onclick="event.stopPropagation(); window.location='{{ $partLink }}'"
-        aria-label="Ga naar de quest {{ $title }}"
+        aria-label="Ga naar de quest {{ $title }} met progressie {{ $progressSplit[0] }} van de {{ $progressSplit[1] }}}"
 
     >
         Ga naar quest
